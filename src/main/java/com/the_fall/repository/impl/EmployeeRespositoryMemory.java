2942891d27808ae8eb16a3aa7c8cf410f3f86cc5
Emanuel Cabrera Novoa <emanuelcabreranovoa@hotmail.com>
2025-09-07 17:52:55 -0500

    implementation for save employees in memory

---
+++ src/main/java/com/the_fall/repository/impl/EmployeeRespositoryMemory.java
@@ -0,0 +1,45 @@
+package src.main.java.com.the_fall.repository.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import src.main.java.com.the_fall.model.Employee;
+import src.main.java.com.the_fall.repository.IEmployeeRepository;
+
+public class EmployeeRespositoryMemory implements IEmployeeRepository {
+  private final List<Employee> employees;
+
+  public EmployeeRespositoryMemory() {
+    this.employees = new ArrayList<>();
+  }
+
+  @Override
+  public void add(Employee e) {
+    if (e == null) {
+      throw new IllegalArgumentException("The employee cannot be null");
+    }
+    employees.add(e);
+  }
+
+  @Override
+  public void update(Employee e) {
+    if (e == null) {
+      throw new IllegalArgumentException("The employee cannot be null");
+    }
+    Optional<Employee> existing = employees.stream().filter(emp -> emp.getId() == e.getId()).findFirst();
+
+    if (existing.isPresent()) {
+      int index = employees.indexOf(existing.get());
+      employees.set(index, e);
+    } else {
+      throw new IllegalArgumentException("Employee with ID " + e.getId() + " not found");
+    }
+  }
+
+  @Override
+  public Employee getById(int id) {
+    return employees.stream().filter(emp -> emp.getId() == id).findFirst()
+        .orElseThrow(() -> new IllegalArgumentException("Employee with ID " + id + " not found"));
+  }
+}
