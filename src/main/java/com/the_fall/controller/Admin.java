1f78ee63469889f2d360a9083ad1f257ec6d184d
Emanuel Cabrera Novoa <emanuelcabreranovoa@hotmail.com>
2025-09-07 17:29:41 -0500

    admin implementation

--- src/main/java/com/the_fall/controller/Admin.java
+++ src/main/java/com/the_fall/controller/Admin.java
@@ -1,5 +1,51 @@
 package src.main.java.com.the_fall.controller;
 
+import java.util.List;
+
+import src.main.java.com.the_fall.model.Employee;
+import src.main.java.com.the_fall.repository.IEmployeeRepository;
+import src.main.java.com.the_fall.repository.ISaleRepository;
+import src.main.java.com.the_fall.service.IPaymantService;
+
 public class Admin {
 
+  private final IEmployeeRepository employeeRepo;
+  private final ISaleRepository saleRepo;
+  private final IPaymantService paymantService;
+
+  public Admin(IEmployeeRepository employeeRepo, ISaleRepository saleRepo, IPaymantService paymantService) {
+    this.employeeRepo = employeeRepo;
+    this.saleRepo = saleRepo;
+    this.paymantService = paymantService;
+  }
+
+  public void registerEmployee(Employee e) {
+    employeeRepo.add(e);
+  }
+
+  public List<Employee> listEmployees() {
+    return employeeRepo.getAll();
+  }
+
+  public void updateEmployee(Employee e) {
+    employeeRepo.update(e);
+  }
+
+  public void deleteEmployee(int id) {
+    employeeRepo.delete(id);
+  }
+
+  public void addSale(double amount) {
+    saleRepo.add(amount);
+  }
+
+  public double getTotalSale() {
+    return saleRepo.getTotal();
+  }
+
+  public void payEmployee(int id, double amount) {
+    Employee e = employeeRepo.getById(id);
+    paymantService.pay(e, amount);
+    employeeRepo.update(e);
+  }
 }
