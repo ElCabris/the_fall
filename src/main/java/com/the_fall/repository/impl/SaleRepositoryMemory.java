183ee2b238c6bf5e7eed45c63814d5510093a62b
Emanuel Cabrera Novoa <emanuelcabreranovoa@hotmail.com>
2025-09-07 16:17:15 -0500

    implementation of sale repository

---
+++ src/main/java/com/the_fall/repository/impl/SaleRepositoryMemory.java
@@ -0,0 +1,27 @@
+package src.main.java.com.the_fall.repository.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import src.main.java.com.the_fall.repository.ISaleRepository;
+
+public class SaleRepositoryMemory implements ISaleRepository {
+  private final List<Double> sales;
+
+  public SaleRepositoryMemory() {
+    this.sales = new ArrayList<>();
+  }
+
+  @Override
+  public void add(double amount) {
+    if (amount <= 0) {
+      throw new IllegalArgumentException("The sale amount must be positive.");
+    }
+    sales.add(amount);
+  }
+
+  @Override
+  public double getTotal() {
+    return sales.stream().mapToDouble(Double::doubleValue).sum();
+  }
+}
