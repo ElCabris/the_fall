d7efa625582c7d7b5cf0e194f4a02a262e14545c
EmanuelOrozco <emanuelorozco2011@hotmail.com>
2025-09-08 02:09:31 -0500

    feat(the_fall): feat App.java

--- src/main/java/com/the_fall/App.java
+++ src/main/java/com/the_fall/App.java
@@ -1,7 +1,22 @@
-package src.main.java.com.the_fall;
+package com.the_fall;
+
+import com.the_fall.controller.Admin;
+import com.the_fall.repository.impl.EmployeeRespositoryMemory;
+import com.the_fall.repository.impl.SaleRepositoryMemory;
+import com.the_fall.service.impl.PaymantServiceBasic;
+import com.the_fall.ui.menus.PrincipalMenu;
 
 public class App {
-  public static void main(String[] args) {
+    public static void main(String[] args) {
+        // Inicializar dependencias
+        var employeeRepo = new EmployeeRespositoryMemory();
+        var saleRepo = new SaleRepositoryMemory();
+        var paymantService = new PaymantServiceBasic();
 
-  }
+        // Controlador
+        var adminController = new Admin(employeeRepo, saleRepo, paymantService);
+
+        // Lanzar menú principal
+        new PrincipalMenu(adminController).handle();
+    }
 }
