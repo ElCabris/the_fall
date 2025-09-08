dbe9df719be068d47ffa74c67b0ca5ecc5b0fb49
Samuel Gallego <samuelgallego441@gmail.com>
2025-09-08 16:10:16 -0500

    feat(model) add atributte on  model Employee.java

--- src/main/java/com/the_fall/model/Employee.java
+++ src/main/java/com/the_fall/model/Employee.java
@@ -1,38 +1,51 @@
 package com.the_fall.model;
 
 public class Employee {
-  private int id;
-  private String name;
-  private double balance;
+    private int id;
+    private String name;
+    private double balance;
+    private String password;
 
-  public Employee(int id, String name) {
-    this.id = id;
-    this.name = name;
-    this.balance = 0;
-  }
-
-  public double getBalace() {
-    return balance;
-  }
-
-  public void wethdrawBalance(double amount) {
-    if (amount <= this.balance) {
-      this.balance -= amount;
-    } else {
-      throw new IllegalArgumentException("Insufficient balance");
+    public Employee(int id, String name, String password) {
+        this.id = id;
+        this.name = name;
+        this.balance = 0;
+        this.password = password;
     }
-  }
 
-  public void deposit(double amount) {
-    this.balance += amount;
-  }
+    public double getBalace() {
+        return balance;
+    }
 
-  public int getId() {
-    return this.id;
-  }
+    public void wethdrawBalance(double amount) {
+        if (amount <= this.balance) {
+            this.balance -= amount;
+        } else {
+            throw new IllegalArgumentException("Saldo insuficiente");
+        }
+    }
 
-  public String getName() {
-    return this.name;
-  }
+    public void deposit(double amount) {
+        this.balance += amount;
+    }
 
+    public int getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean verifyPassword(String inputPassword) {
+        return this.password.equals(inputPassword);
+    }
 }
